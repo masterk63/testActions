897495440a20d2b2d223934b462b3c6d1cf54f30
MasterK <masterk@MacBook-Pro-de-MasterK.local>
2020-06-03 16:42:28 -0300

    add mongoDB

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './src/bin/www.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: '12',
+                },
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  resolveLoader: {
+    modules: [__dirname + '/node_modules'],
+  },
+};
